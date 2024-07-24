02eafadacd98d5c5596089b6ba05b0dff8dfc180
aureezzhenx <jouzie.aurez@gmail.com>
2024-07-24 17:46:21 +0700

    fixing versioning

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const process = require("process");
-let version = process.env.VERSION_BUILD;
+let versibuild = process.env.VERSION_BUILD;
 
-if (!version) {
-    version = "1";
+if (!versibuild) {
+    versibuild = "1";
 }
 
 const hostname = '0.0.0.0';
@@ -12,7 +12,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Application Version:') + version;
+    res.end('Application Version: ') + versibuild;
 });
 
 server.listen(port, hostname, () => {
