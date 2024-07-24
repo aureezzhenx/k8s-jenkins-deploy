3243b4b6e3671e396424489c4ffa5e101e98ed0a
aureezzhenx <jouzie.aurez@gmail.com>
2024-07-24 18:04:45 +0700

    versioning

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
 const http = require('http');
-const process = require("process");
-let versibuild = process.env.VERSION_BUILD;
-
-if (!versibuild) {
-    versibuild = "1";
-}
-
-console.log(process.env);
 
 const hostname = '0.0.0.0';
 const port = 3000;
@@ -14,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('OKE MANTAP');
+    res.end('Batch #45 Started');
 });
 
 server.listen(port, hostname, () => {
